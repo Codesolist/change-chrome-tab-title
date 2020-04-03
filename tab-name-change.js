e430af2b9e2f20550dc25bc2b9fa436e77881e3d
Alp Eren Güney <62503761+Codesolist@users.noreply.github.com>
2020-04-04 00:45:16 +0300

    Create tab-name-change.js

---
+++ tab-name-change.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+var title = document.title;
+var alttitle = "Buraya gelir misin lütfen";
+window.onblur = function () { document.title = alttitle; };
+window.onfocus = function () { document.title = title; };
+</script>
